c40c7a4fed953ce86b90277a92faa77eebd445a6
unknown <owaisraza54@yahoo.com>
2019-02-20 09:51:39 -0800

    deletedkey

--- tutotrial 1/main.js
+++ tutotrial 1/main.js
@@ -1,5 +1,5 @@
-  const APP_ID = '6E1C9799-02E9-67E1-FFB2-3F35D785E300';
-  const API_KEY = 'AA9F0B46-839A-EB63-FF68-33FF3CCE1E00';
+  const APP_ID = '';
+  const API_KEY = '';
 
   Backendless.serverURL = 'https://api.backendless.com';
   Backendless.initApp(APP_ID, API_KEY);
