3f78c9b8cd48aa816e1ec02dc9a59c673491ce2a
unknown <owaisraza54@yahoo.com>
2019-02-28 08:28:27 -0800

    third tutorial 1

--- tutorial 3/firstapp/src/js/main.js
+++ tutorial 3/firstapp/src/js/main.js
@@ -1,3 +1,10 @@
+
+  const APP_ID = '';
+  const API_KEY = '';
+
+  Backendless.serverURL = 'https://api.backendless.com';
+  Backendless.initApp(APP_ID, API_KEY);
+
 function userRegistered( user )
 {
   console.log( user+"user has been registered" );
@@ -9,8 +16,23 @@ function gotError( err ) // see more on error handling
   console.log( "error code - " + err.statusCode );
 }
 
-var user = new Backendless.User();
-user.email = "james.bond@mi6.co.uk";
-user.password = "iAmWatchingU";
 
-Backendless.UserService.register( user ).then( userRegistered ).catch( gotError );
+document.getElementById("btn").addEventListener("click", function(event){
+  event.preventDefault();
+  var user = new Backendless.User();
+  user.email = document.getElementById('email').value;
+  user.password = document.getElementById('pass').value;
+
+  Backendless.UserService.register( user ).then( userRegistered ).catch( gotError );
+});
+
+
+document.getElementById("login").addEventListener("click", function(event){
+  event.preventDefault();
+  Backendless.UserService.login( document.getElementById('lemail').value, document.getElementById('lpass').value )
+ .then( function( loggedInUser ) {
+  console.log(loggedInUser+' is logged in')
+   })
+ .catch( function( error ) {
+   });
+});
